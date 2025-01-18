fca762805f8b45657dc9d247a567b902ca9fc8f2
jakub.bednarek <jakub.bednare1009@gmail.com>
2025-01-18 18:28:36 +0100

    Created MLPClassifier processing function

--- mlp.py
+++ mlp.py
@@ -1,9 +1,10 @@
 #! /bin/python3
 
-import io
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import cross_val_score
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.neural_network import MLPClassifier
 
 DATA_FILE_PATH = "./data/data.csv"
 
@@ -14,18 +15,28 @@ def read_and_preprocess_data(path):
     data['diagnosis'] = [ 0 if value == 'M' else 1 for value in data['diagnosis'] ]
 
     scaler = MinMaxScaler(feature_range=(0, 1), copy=False)
-    data = scaler.fit(data).transform(data)
+    data = scaler.fit_transform(data)
 
     diagnosis = np.array([ entry[0] for entry in data ])
-    parameters = np.matrix([ entry[1:-1] for entry in data ])
+    parameters = np.array([ entry[1:-1] for entry in data ])
 
     return (diagnosis, parameters)
+
+def perform_mlp_classification(diagnosis_data, parameters):
+    hidden_layer_sizes = [(30,), (30, 30), (45, 70), (60,), (60, 60), (100,), (10, 40)]
+    scores = {}
+
+    for layer in hidden_layer_sizes:
+        mlp = MLPClassifier(hidden_layer_sizes=layer, random_state=1410, max_iter=400)
+        score = cross_val_score(mlp, parameters, diagnosis_data, scoring='balanced_accuracy').mean()
+        scores[layer] = score
+        
+    return scores
 
 def main():
     diagnosis, parameters = read_and_preprocess_data(DATA_FILE_PATH)
 
-    print(diagnosis)
-    print(parameters)
+    perform_mlp_classification(diagnosis_data=diagnosis, parameters=parameters)
 
 if __name__ == "__main__":
     main()
