f0f0f2d5067b5afe8ab6a6b4fbb6041fe044e9d0
jakub.bednarek <jakub.bednare1009@gmail.com>
2025-01-18 18:41:51 +0100

    Added mlp classification processing for activation method

--- mlp.py
+++ mlp.py
@@ -8,6 +8,16 @@ from sklearn.neural_network import MLPClassifier
 
 DATA_FILE_PATH = "./data/data.csv"
 
+def get_best_layer_parameter(input_dict):
+    best_layer = ()
+    best_score = 0
+    for layer, score in input_dict.items():
+        if score > best_score:
+            best_score = score
+            best_layer = layer
+
+    return best_layer
+
 def read_and_preprocess_data(path):
     data = pd.read_csv(path)
     data = data.drop('id', axis=1)
@@ -22,8 +32,8 @@ def read_and_preprocess_data(path):
 
     return (diagnosis, parameters)
 
-def perform_mlp_classification(diagnosis_data, parameters):
-    hidden_layer_sizes = [(30,), (30, 30), (45, 70), (60,), (60, 60), (100,), (10, 40)]
+def perform_mlp_classification_by_hiden_layer(diagnosis_data, parameters):
+    hidden_layer_sizes=[(30,), (30, 30), (45, 70), (60,), (60, 60), (100,), (10, 40)]
     scores = {}
 
     for layer in hidden_layer_sizes:
@@ -33,10 +43,31 @@ def perform_mlp_classification(diagnosis_data, parameters):
         
     return scores
 
+def perform_mlp_classification_activation(diagnosis_data, parameters, hidden_layer_size):
+    activation_methods = ["relu", "identity", "logistic", "tanh"]
+    scores = {}
+
+    for method in activation_methods:
+        mlp = MLPClassifier(hidden_layer_sizes=hidden_layer_size, random_state=1410, max_iter=400, activation=method)
+        score = cross_val_score(mlp, parameters, diagnosis_data, scoring='balanced_accuracy').mean()
+        scores[method] = score
+        
+    return scores
+
 def main():
     diagnosis, parameters = read_and_preprocess_data(DATA_FILE_PATH)
 
-    perform_mlp_classification(diagnosis_data=diagnosis, parameters=parameters)
+    hidden_layer_scores = perform_mlp_classification_by_hiden_layer(diagnosis_data=diagnosis, parameters=parameters)
+    best_hidden_layer_classification_layer = get_best_layer_parameter(hidden_layer_scores)
+    
+    print(hidden_layer_scores)
+    print(best_hidden_layer_classification_layer)
+
+    activation_scores = perform_mlp_classification_activation(diagnosis_data=diagnosis, parameters=parameters, hidden_layer_size=best_hidden_layer_classification_layer)
+    best_activation_classification_activation = get_best_layer_parameter(activation_scores)
+    
+    print(activation_scores)
+    print(best_activation_classification_activation)
 
 if __name__ == "__main__":
     main()
